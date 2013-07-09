3110604d89bf795cc1f33f602695f5333cf35c9d
fathoni <fathoni.am@gmail.com>
2013-07-09 14:29:38 +0000

    add index.html and modify web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs = require('fs');
+    //var buf = new Buffer();                                                   
+    var fileBuff =  new Buffer(fs.readFileSync('index.html'));
+    var finalContent =fileBuff.toString("utf-8");
+    response.send(finalContent);
 });
 
 var port = process.env.PORT || 5000;
